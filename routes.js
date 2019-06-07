0502f0e88c84f01756170cddd5a561e693dd562d
Matias Perez Hughes <hughesmatias@gmail.com>
2019-06-07 20:10:30 -0300

    users router

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var router = express.Router();
+var userRoutes = require('./users');
 
 router.get('/', (req, res) => {
   res.send('REST API. Init');
 });
+
+router.use('/users', userRoutes);
 
 module.exports = router;
