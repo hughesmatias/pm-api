9eedb2d3641cf7f2680f5684ba6a82e6cf4f8504
Matias Perez Hughes <hughesmatias@gmail.com>
2019-06-07 20:04:09 -0300

    implement main routes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-
 var app = express();
+var routes = require('./routes'); 
 
-app.get('/', (req, res) => {
-  res.send('RESTful API.');
-});
+app.use('/', routes);
 
 app.listen(3000, () => {
   console.log(`Running in 3000`);
