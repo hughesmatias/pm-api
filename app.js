33269d696e56a04ff0724fd758da26512e784bae
Matias Perez Hughes <hughesmatias@gmail.com>
2019-06-09 16:01:21 -0300

    call mongo db. And add the get data from db (first implementation)

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 var routes = require('./routes'); 
 
-app.use('/', routes);
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3000, () => {
-  console.log(`Running in 3000`);
+MongoClient.connect('mongodb://root:root123@ds135207.mlab.com:35207/peer-medical', (err, result) => {
+  const db = result.db('peer-medical');
+
+  app.use('/', routes(db));
+
+  app.listen(3000, () => {
+    console.log(`Running in 3000`);
+  });
 });
--- routes.js
+++ routes.js
@@ -2,10 +2,16 @@ var express = require('express')
 var router = express.Router();
 var userRoutes = require('./users');
 
-router.get('/', (req, res) => {
-  res.send('REST API. Init');
-});
+const configRouters = (db) => {
+  router.get('/', (req, res) => {
+    console.log(db.collection('users').find(), 'aqui campeon');
+    // res.send('REST API. Init');
+    res.json(db.collection('users').find())
+  });
+  
+  router.use('/users', userRoutes);
 
-router.use('/users', userRoutes);
+  return router;
+}
 
-module.exports = router;
+module.exports = configRouters;
