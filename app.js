5b3534e7341d512385ccdf4a2ee34d9bb8bbdab3
Matias Perez Hughes <hughesmatias@gmail.com>
2019-06-21 00:58:02 -0300

    parsing middleware

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 const userSchema = require('./user/model');
 const articleSchema = require('./article/model');
 
 mongoose.connect('mongodb://root:root123@ds135207.mlab.com:35207/peer-medical', {
   useNewUrlParser: true,
 }).then(() => {
+  app.use(bodyParser.json());
+
   mongoose.model('user', userSchema);
   mongoose.model('article', articleSchema);
   app.use('/', routes(mongoose));
