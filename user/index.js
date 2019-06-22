90e71afbda94bfbd50fb14c4260c930dd4c71ea8
Matias Perez Hughes <hughesmatias@gmail.com>
2019-06-22 16:35:37 -0300

    get user by id. And refator model class name

--- user/index.js
+++ user/index.js
@@ -2,16 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 const userRouters = (db) => {
-  var users = db.model('user');
+  var User = db.model('user');
 
   router.get('/', async (req, res) => {
-    const usersCollection = await users.find().then(data => data);
+    const usersCollection = await User.find().then(data => data);
     res.json(usersCollection);
   });
 
+  router.get('/:id', async(req, res) => {
+    const userId = req.params.id;
+    User.findOne({ _id: userId }).then(user => {
+      res.json(user);
+    })
+    .catch(err => {
+      res.end("User not found");
+    })
+  })
+
   router.post('/', async (req, res) => {
-    const newUser = new users(req.body);
-    const userAdded = await users.collection.insertOne(newUser, (err, result) => result | err);
+    const newUser = new User(req.body);
+    const userAdded = await User.collection.insertOne(newUser, (err, result) => result | err);
     res.json(userAdded);
   });
 
