c96270ea1c465eb812f9edceb0a019fee88c99d3
Matias Perez Hughes <hughesmatias@gmail.com>
2019-06-25 21:59:32 -0300

    create middleware to check if the request contains authorization token

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ const articleSchema = require('./article/model');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const config =  require('./config');
+
+const accessMiddleware = (req, res, next) => {
+  // validate if request header authorization
+  const authotirzationHeader = req.get('Authorization');
+  if (authotirzationHeader != undefined && authotirzationHeader == config.token) {
+    return next();
+  } else {
+    res.end('Error token');
+  }
+}
+
 app.use(bodyParser.json());
+app.use(accessMiddleware);
 
 mongoose.connect('mongodb://root:root123@ds135207.mlab.com:35207/peer-medical', {
   useNewUrlParser: true,
